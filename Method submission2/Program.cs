7882beb0d725eb7a91b5ade9e894fa36a6cd3a6a
robsevcik <robert.a.sevcik@gmail.com>
2021-06-17 16:31:14 -0700

    Create Program.cs

---
+++ Method submission2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Method_submission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            optional1 optional1 = new optional1();
+
+            Console.WriteLine("input a number");
+            Console.ReadLine();
+
+            Console.WriteLine("input a second number or nothing");
+            Console.ReadLine();
+
+             static void AddNumbers(numberone, numbertwo);
+        }
+    }
+}
