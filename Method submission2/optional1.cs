acc6105b58e7c24b2d4a382b0ec6f018af21c720
robsevcik <robert.a.sevcik@gmail.com>
2021-06-17 16:31:01 -0700

    Create optional1.cs

---
+++ Method submission2/optional1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_submission2
+{
+    class optional1
+    {
+        public static void AddNumbers(int firstnumber, int[] secondnumber);
+        
+        int result = firstnumber + 5;
+        
+           Console.WriteLine(result);
+
+
+    }
+}
