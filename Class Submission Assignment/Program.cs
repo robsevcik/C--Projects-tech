85bc10be6e9a3181a4947b3b598a182e6e2c3f2e
robsevcik <robert.a.sevcik@gmail.com>
2021-06-20 14:14:13 -0700

    Create Program.cs

---
+++ Class Submission Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Class_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Class1 = new Class1();
+
+            Class1.Method1();
+
+            Console.WriteLine("enter a number");
+          int1 = Console.ReadLine();
+            Console.WriteLine(one);
+           
+
+
+
+        }
+    }
+}
