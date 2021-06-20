1382dd39f01230cb4c6a23fd4262fe0660bdcc29
robsevcik <robert.a.sevcik@gmail.com>
2021-06-20 15:37:13 -0700

    Update Program.cs

--- Class Submission Assignment/Program.cs
+++ Class Submission Assignment/Program.cs
@@ -6,14 +6,24 @@ namespace Class_Submission_Assignment
     {
         static void Main(string[] args)
         {
-            Class1 Class1 = new Class1();
+            //instatiation object of class 1 named obj 1//
+            Class1 obj1 = new Class1();
+            //display a message, read user input into int//
+            Console.WriteLine(" enter a number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            //calls the method1 method through obj1//
+            obj1.Method1(y);
+            int num;
+            //calls the method2 method through obj1//
+            obj1.Method2(out num);
+            Console.WriteLine(num);
+            string text = "a value";
+            //calls overloaded version of method1//
+            obj1.Method1(text);
+            Class2.Method1();
 
-            Class1.Method1();
+            Console.ReadLine();
 
-            Console.WriteLine("enter a number");
-          int1 = Console.ReadLine();
-            Console.WriteLine(one);
-           
 
 
 
