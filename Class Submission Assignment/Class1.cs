d1b6a1dc96c65b80c3c2b182d4b6227feb9fda37
robsevcik <robert.a.sevcik@gmail.com>
2021-06-20 14:14:40 -0700

    Create Class1.cs

---
+++ Class Submission Assignment/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    class Class1
+    {
+        public void  Method1(int int1);
+    }
+    int int1 = Console.ReadLine();
+    int one = int1 / 2;
+
+    return one;
+
+}
