5bfe21ea13f62fbd89cfeaf45538c1e962dd11e7
robsevcik <robert.a.sevcik@gmail.com>
2021-06-24 16:50:46 -0700

    Create Program.cs

---
+++ Methods and objects submission assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Methods_and_objects_submission_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firstname first1 = new Firstname("Sample");
+            Lastname last1 = new Firstname("Student");
+        }
+    }
+}
