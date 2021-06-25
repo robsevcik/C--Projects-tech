9f314dc04a95950740508596a3c654ec6f360ac6
robsevcik <robert.a.sevcik@gmail.com>
2021-06-25 07:58:43 -0700

    Update Program.cs

--- Methods and objects submission assignment/Program.cs
+++ Methods and objects submission assignment/Program.cs
@@ -7,8 +7,10 @@ namespace Methods_and_objects_submission_assignment
         static void Main(string[] args)
         {
             Employee employee = new Employee();
+            //instantiate and initialize an employee object//
             employee.Firstname = "Sample";
             employee.Lastname = "Student";
+            //calling the method//
             employee.SayName();
 
         }
