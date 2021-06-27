9d8385b0865f70c448bc61620c2c7fa5a232ab76
robsevcik <robert.a.sevcik@gmail.com>
2021-06-27 16:56:23 -0700

    Update Person.cs

--- Abstract Class Submission Assignment/Person.cs
+++ Abstract Class Submission Assignment/Person.cs
@@ -8,14 +8,13 @@ namespace Abstract_Class_Submission_Assignment
 {
     public abstract class Person
     {
+        //properties//
         public string Firstname { get; set; }
         public string Lastname { get; set; }
 
-        public static SayName();
+        public void SayName();
 
-        public override string ToString()
-        {
-            return base.ToString();
+        
         }
     }
 }
