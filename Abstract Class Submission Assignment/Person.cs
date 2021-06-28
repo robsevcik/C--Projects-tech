55fecec5326aeaeba7a09eaf64113394d1d8a371
robsevcik <robert.a.sevcik@gmail.com>
2021-06-28 09:14:39 -0700

    Update Person.cs

--- Abstract Class Submission Assignment/Person.cs
+++ Abstract Class Submission Assignment/Person.cs
@@ -12,9 +12,9 @@ namespace Abstract_Class_Submission_Assignment
         public string Firstname { get; set; }
         public string Lastname { get; set; }
 
-        public void SayName();
+        public abstract void SayName();
 
-        
-        }
+
     }
 }
+
