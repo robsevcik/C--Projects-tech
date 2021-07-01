4a84a705ab91725f041301949d6a6c0e825a1456
robsevcik <robert.a.sevcik@gmail.com>
2021-07-01 10:33:18 -0700

    Create Employee.cs

---
+++ Operators Submission Assignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission_Assignment
+{
+    class Employee
+    {
+    }
+}
