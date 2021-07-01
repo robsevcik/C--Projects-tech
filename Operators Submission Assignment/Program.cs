d2e1598cc345399099682c5726922eacce8b7792
robsevcik <robert.a.sevcik@gmail.com>
2021-07-01 10:32:51 -0700

    Create Program.cs

---
+++ Operators Submission Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operators_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj1 = new Employee();
+            Employee obj2 = new Employee();
+        }
+
+        public static bool operator ==(obj1, obj2)
+        { 
+
+    Console.WriteLine(obj1 == obj2);
+    }
+}
