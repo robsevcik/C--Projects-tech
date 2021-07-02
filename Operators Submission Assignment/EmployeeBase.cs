6d0ab7afccc6ec597fe14374a9c809a807c59f6c
robsevcik <robert.a.sevcik@gmail.com>
2021-07-01 17:18:13 -0700

    Create EmployeeBase.cs

---
+++ Operators Submission Assignment/EmployeeBase.cs
@@ -0,0 +1,21 @@
+﻿namespace Operators_Submission_Assignment
+{
+    public class EmployeeBase
+    {
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
