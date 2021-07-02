3ac15a0abac916f5a889ca2d26f7f0aa6b648a4f
robsevcik <robert.a.sevcik@gmail.com>
2021-07-01 18:23:22 -0700

    Update Employee.cs

--- Operators Submission Assignment/Employee.cs
+++ Operators Submission Assignment/Employee.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace Operators_Submission_Assignment
 {
-    public class Employee 
+    public class Employee
     {
         public int ID { get; set; }
-        public int ID1 { get; set; }
 
+        
         public static bool operator ==(Employee obj1, Employee obj2);
+        {
+        if (obj1 == obj2);
+            return true;
+            }
+
+    else { return false}
+
+
     }
 }
