e21eaa17b2ec7c88116a30b0008b88d41ede7b1a
robsevcik <robert.a.sevcik@gmail.com>
2021-07-04 14:08:41 -0700

    Create Employee.cs

---
+++ Parameters Submission Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Submission_Assignment
+{
+    class Employee<T>
+    {
+        public string Things  { get; set; }
+    
+       
+    }
+}
