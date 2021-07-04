1d61e7d4ca2a446d83b5a72e1696c03117ace266
robsevcik <robert.a.sevcik@gmail.com>
2021-07-04 14:08:19 -0700

    Create Program.cs

---
+++ Parameters Submission Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Parameters_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee; string obj1 = new Employee();
+
+            List<string> things = new List<string>();
+
+            things.Add("cat");
+            things.Add("dog");
+            things.Add("boy");
+
+            Employee; int obj2 = new Employee();
+
+            things.Add(3);
+            things.Add(5);
+            things.Add(0235);
+
+        }
+    }
+
+}
