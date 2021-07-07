5c2e9c83812e8726f542d847b20a2c3104678c50
robsevcik <robert.a.sevcik@gmail.com>
2021-07-07 11:28:42 -0700

    Update Program.cs

--- Parameters Submission Assignment/Program.cs
+++ Parameters Submission Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Parameters_Submission_Assignment
 {
@@ -8,7 +9,7 @@ namespace Parameters_Submission_Assignment
         {
             Employee<string> obj1 = new Employee<string>();
 
-            List<string> things = new List<string>();
+          
 
             obj1.Things = new List<string> { "cat", "dog", "boy" };
 
@@ -17,10 +18,15 @@ namespace Parameters_Submission_Assignment
             obj2.Things = new List<int> { 5, 3, 523 };
 
             //loop through list with a for loop//
-            for (int i = 0; i < List.Count; i++)
+            for (int i = 0; i < obj1.Things.Count; i++)
             {
-                Console.WriteLine(List[i]);
+                Console.WriteLine(obj1.Things);
             }
+            for (int c = 0; c < obj2.Things.Count; c++) 
+            {
+                Console.WriteLine(obj2.Things);
+            }
+            
         }
     }
 
