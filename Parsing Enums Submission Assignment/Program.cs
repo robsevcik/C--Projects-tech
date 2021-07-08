100cb397526cb2bee999f7fe79e8d621741b8001
robsevcik <robert.a.sevcik@gmail.com>
2021-07-08 16:44:11 -0700

    Update Program.cs

--- Parsing Enums Submission Assignment/Program.cs
+++ Parsing Enums Submission Assignment/Program.cs
@@ -5,7 +5,7 @@ namespace Parsing_Enums_Submission_Assignment
     public class Program
     {
 
-        static void Main(string[] args);
+     public static void Main(string[] args);
     }
     public enum Day
     {
@@ -13,45 +13,73 @@ namespace Parsing_Enums_Submission_Assignment
         Tuesday,
         Wednesday,
         Thursday,
-        Friday,
+        Friday= 10,
         Saturday,
-        Sunday
+        Sunday,
 
+    
+           
+
+
+    Console,WriteLine("enter the day of the week"),
+
+        i = Console.ReadLine();
     }
 
-    Console.WriteLine("enter the day of the week");
-
-            Day = Console.ReadLine();
-
-        string name = null;
-    if(string.IsNullOrEmpty(name);
-    {
-    throw new SevcikCustomException("please enter a day of the week");
-    }
-    else
+        try
 {
-    Console.WriteLine("Day: " + name);
-
+    i != enum.Day;
 }
-    Console.ReadLine();
-}
-public class SevcikCustomException: Exception
-    public SevcikCustomException()
+        catch (Exception e)
 {
+    Console.WriteLine("Please enter an actual day of the week");
+}
 
 }
-public SevcikCustomException(string message) : base(message)
-    {
 
-}
-public SevcikCustomException(string message,Exception innerexception) : base(message, innerexception)
-    {
 
-}
+
+
+
+
+
+
+
+//    foreach (string val in Daylist) {
+//Day day;
+//if (Enum.TryParse(val, true, out day))
+//    if (Enum.IsDefined(typeof(Day), day) | vehicle.ToString()
+//        .Contains(","))
+//        Console.WriteLine
+    //    string name = null;
+    //    if (string.IsNullOrEmpty(name))
+    //            {
+    //        throw new SevcikCustomException("please enter a day of the week");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Day: " + name);
+
+    //    }
+    //    Console.ReadLine();
+    //        }
+    //public class SevcikCustomException: Exception { 
+    //    public SevcikCustomException()
+    //{
+
+    //}
+    //public SevcikCustomException(string message) : base(message)
+    //    {
+
+    //}
+    //public SevcikCustomException(string message,Exception innerexception) : base(message, innerexception)
+    //    {
+
+    //}
 
  
-}
-}
+
+
 
 
 
