141f7d7bd66c32e9e7b6515009f3848dd6dfcc5f
robsevcik <robert.a.sevcik@gmail.com>
2021-07-08 13:33:12 -0700

    Create Program.cs

---
+++ Parsing Enums Submission Assignment/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Parsing_Enums_Submission_Assignment
+{
+    public class Program
+    {
+
+        static void Main(string[] args);
+    }
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+    }
+
+    Console.WriteLine("enter the day of the week");
+
+            Day = Console.ReadLine();
+
+        string name = null;
+    if(string.IsNullOrEmpty(name);
+    {
+    throw new SevcikCustomException("please enter a day of the week");
+    }
+    else
+{
+    Console.WriteLine("Day: " + name);
+
+}
+    Console.ReadLine();
+}
+public class SevcikCustomException: Exception
+    public SevcikCustomException()
+{
+
+}
+public SevcikCustomException(string message) : base(message)
+    {
+
+}
+public SevcikCustomException(string message,Exception innerexception) : base(message, innerexception)
+    {
+
+}
+
+ 
+}
+}
+
+
+
+
+
+
