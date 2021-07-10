d981f2a3e1650917c13c8a3d30f90015a4816d58
robsevcik <robert.a.sevcik@gmail.com>
2021-07-10 11:00:59 -0700

    Create Program.cs

---
+++ Struct challenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Struct_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+
+            number obj1 = a;
+
+
+                   struct Amount
+            {
+
+                public decimal a;
+                public decimal b;
+            }
+
+        }
+    }
+}
