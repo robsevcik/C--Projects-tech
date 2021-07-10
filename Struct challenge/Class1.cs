28cff199f8a7d509cd950cba0c184da96639eaad
robsevcik <robert.a.sevcik@gmail.com>
2021-07-10 15:44:40 -0700

    Create Class1.cs

---
+++ Struct challenge/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_challenge
+{
+    class Class1
+    {
+
+       public struct Number
+        {
+
+            public decimal Amount { get; set; }
+            
+        }
+    }
+}
